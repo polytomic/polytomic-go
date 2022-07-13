53e6c40afc47b3cd5fc7aafe9e6c2a0d41eaf085
Nathan Yergler <nathan@yergler.net>
2022-07-13 11:43:06 -0700

    Return server-provided errors from client methods
    Closes #1.

--- client.go
+++ client.go
@@ -1,6 +1,15 @@
 package polytomic
 
-import "github.com/carlmjohnson/requests"
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/carlmjohnson/requests"
+)
 
 type Client struct {
 	host          string
@@ -37,9 +46,53 @@ func (c *Client) newRequest(url string) *requests.Builder {
 		URL(url).
 		Host(c.host).
 		BasicAuth(c.deploymentKey, "").
-		UserAgent("polytomic-go")
+		UserAgent("polytomic-go").
+		AddValidator(checkApiResponse)
 }
 
 type topLevelResult struct {
 	Result interface{} `json:"result"`
+	Error  interface{} `json:"error"`
+}
+
+// checkApiResponse provides a request.ResponseHandler which will attempt to
+// parse non-2xx responses as topLevelResults.
+func checkApiResponse(res *http.Response) error {
+	if res.StatusCode < 400 {
+		return nil
+	}
+
+	result := topLevelResult{}
+
+	buf := &bytes.Buffer{}
+	defer func() {
+		res.Body = io.NopCloser(buf)
+	}()
+	body, err := io.ReadAll(io.TeeReader(res.Body, buf))
+	defer res.Body.Close()
+	if err != nil {
+		return err
+	}
+
+	if json.Unmarshal(body, &result) != nil || result.Error == nil {
+		// this wasn't a top level result
+		return errors.New(fmt.Sprintf("unexpected error (%d): %s", res.StatusCode, string(body)))
+	}
+	if errmap, ok := result.Error.(map[string]interface{}); ok {
+		if msg, ok := errmap["message"]; ok {
+			if msgStr, ok := msg.(string); ok {
+				return ApiError{message: msgStr, statusCode: res.StatusCode}
+			}
+		}
+	}
+	return nil
+}
+
+type ApiError struct {
+	statusCode int
+	message    string
+}
+
+func (e ApiError) Error() string {
+	return fmt.Sprintf("%s (%d)", e.message, e.statusCode)
 }

6c5d6f48f509a9293aaff01b19c5fa0bb48b7efc
Jake Neyer <jake@polytomic.com>
2022-10-25 20:55:15 -0400

    Add s3 connection (#4)

--- connection_type.go
+++ connection_type.go
@@ -4,7 +4,15 @@ const (
 	AthenaConnectionType     = "awsathena"
 	PostgresqlConnectionType = "postgresql"
 	SQLServerConnectionType  = "azuresql"
+	S3ConnectionType         = "s3"
 )
+
+type S3Configuration struct {
+	AccessKeyID     string `json:"aws_access_key_id"`
+	AccessKeySecret string `json:"aws_secret_access_key"`
+	Region          string `json:"s3_bucket_region"`
+	Bucket          string `json:"s3_bucket_name"`
+}
 
 type AthenaConfiguration struct {
 	AccessKeyID     string `json:"access_id"`

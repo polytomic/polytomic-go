a1acfd93c97b467db868947fed3b1f6c60bde68c
Jake Neyer <jake@polytomic.com>
2022-11-11 09:18:30 -0500

    Add salesforce, hubspot, googleads, gsheets, snowflake, and extend postgres config

--- connection_types.go
+++ connection_types.go
@@ -8,7 +8,48 @@ const (
 	AzureBlobConnectionType          = "azureblob"
 	GoogleCloudStorageConnectionType = "gcs"
 	BigQueryConnectionType           = "bigquery"
+	GoogleSheetsConnectionType       = "gsheets"
+	GoogleAdsConnectionType          = "googleads"
+	SalesforceConnectionType         = "salesforce"
+	HubspotConnectionType            = "hubspot"
+	SnowflakeConnectionType          = "snowflake"
 )
+
+type SnowflakeConfiguration struct {
+	Account          string `json:"account"`
+	Username         string `json:"username"`
+	Password         string `json:"password"`
+	Database         string `json:"dbname"`
+	Warehouse        string `json:"warehouse"`
+	AdditionalParams string `json:"params"`
+}
+
+type HubspotConnfiguration struct {
+	HubDomain string `json:"hub_domain"`
+	HubId     int    `json:"hub_id"`
+}
+
+type GoogleAdsConfiguration struct {
+	ResourceId struct {
+		Value string `json:"value"`
+		Label string `json:"label"`
+	} `json:"resource_id"`
+}
+
+type GoogleSheetsConfiguration struct {
+	UserEmail     string `json:"user_email"`
+	SpreadsheetID struct {
+		Value string `json:"value"`
+		Label string `json:"label"`
+	} `json:"spreadsheet_id"`
+}
+
+type SalesforceConfiguration struct {
+	Username    string `json:"username"`
+	Domain      string `json:"domain"`
+	InstanceURL string `json:"instance_url"`
+	APIVersion  int    `json:"api_version"`
+}
 
 type BigQueryConfiguration struct {
 	ServiceAccountCredentials string `json:"service_account"`
@@ -56,4 +97,24 @@ type PostgresqlConfiguration struct {
 	Database string `json:"database"`
 	Port     int    `json:"port"`
 	SSL      bool   `json:"ssl"`
+
+	// Client Certificates
+	ClientCerts       bool   `json:"client_certs"`
+	ClientCertificate string `json:"client_certificate"`
+	ClientKey         string `json:"client_key"`
+	CACert            string `json:"ca_cert"`
+
+	// Change Detection
+	ChangeDetection bool   `json:"change_detection"`
+	Publication     string `json:"publication"`
+
+	SQLTunnelConfiguration
+}
+
+type SQLTunnelConfiguration struct {
+	SSH        bool   `json:"ssh"`
+	SSHUser    string `json:"ssh_user,omitempty"`
+	SSHHost    string `json:"ssh_host,omitempty"`
+	SSHPort    int    `json:"ssh_port,omitempty"`
+	PrivateKey string `json:"ssh_private_key,omitempty"`
 }

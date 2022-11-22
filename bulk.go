e54ed561d93dc0ff6a031a4f0b136ca554f5166d
Jake Neyer <jake@polytomic.com>
2022-11-22 08:09:15 -0500

    Support bulk api with separate routes for schemas (#10)

--- bulk.go
+++ bulk.go
@@ -10,7 +10,7 @@ type BulkSyncRequest struct {
 	DestConnectionID         string                 `json:"destination_connection_id"`
 	SourceConnectionID       string                 `json:"source_connection_id"`
 	Mode                     string                 `json:"mode"`
-	Schemas                  []BulkSchema           `json:"schemas"`
+	Schemas                  []string               `json:"schemas,omitempty"`
 	Discover                 bool                   `json:"discover"`
 	Active                   bool                   `json:"active"`
 	Schedule                 Schedule               `json:"schedule"`
@@ -19,12 +19,12 @@ type BulkSyncRequest struct {
 }
 
 type Schedule struct {
-	Frequency  *string `json:"frequency" tfsdk:"frequency"`
-	DayOfWeek  *string `json:"day_of_week" tfsdk:"day_of_week"`
-	Hour       *string `json:"hour" tfsdk:"hour"`
-	Minute     *string `json:"minute" tfsdk:"minute"`
-	Month      *string `json:"month" tfsdk:"month"`
-	DayOfMonth *string `json:"day_of_month" tfsdk:"day_of_month"`
+	Frequency  *string `json:"frequency,omitempty" tfsdk:"frequency"`
+	DayOfWeek  *string `json:"day_of_week,omitempty" tfsdk:"day_of_week"`
+	Hour       *string `json:"hour,omitempty" tfsdk:"hour"`
+	Minute     *string `json:"minut,omitempty" tfsdk:"minute"`
+	Month      *string `json:"month,omitempty" tfsdk:"month"`
+	DayOfMonth *string `json:"day_of_month,omitempty" tfsdk:"day_of_month"`
 }
 
 type BulkSchema struct {
@@ -46,7 +46,7 @@ type Mode struct {
 }
 
 type BulkSource struct {
-	Schemas       []Schema    `json:"schemas" tfsdk:"schemas"`
+	Schemas       []string    `json:"schemas" tfsdk:"schemas"`
 	Configuration interface{} `json:"configuration" tfsdk:"configuration"`
 }
 
@@ -87,15 +87,14 @@ func (b *BulkApi) GetDestination(ctx context.Context, connID string) (*BulkDesti
 }
 
 type BulkSyncResponse struct {
-	ID                 string       `json:"id" tfsdk:"id"`
-	Name               string       `json:"name" tfsdk:"name"`
-	DestConnectionID   string       `json:"destination_connection_id" tfsdk:"destination_connection_id"`
-	SourceConnectionID string       `json:"source_connection_id" tfsdk:"source_connection_id"`
-	Mode               string       `json:"mode" tfsdk:"mode"`
-	Schemas            []BulkSchema `json:"schemas" tfsdk:"schemas"`
-	Discover           bool         `json:"discover" tfsdk:"discover"`
-	Active             bool         `json:"active" tfsdk:"active"`
-	Schedule           Schedule     `json:"schedule" tfsdk:"schedule"`
+	ID                 string   `json:"id" tfsdk:"id"`
+	Name               string   `json:"name" tfsdk:"name"`
+	DestConnectionID   string   `json:"destination_connection_id" tfsdk:"destination_connection_id"`
+	SourceConnectionID string   `json:"source_connection_id" tfsdk:"source_connection_id"`
+	Mode               string   `json:"mode" tfsdk:"mode"`
+	Discover           bool     `json:"discover" tfsdk:"discover"`
+	Active             bool     `json:"active" tfsdk:"active"`
+	Schedule           Schedule `json:"schedule" tfsdk:"schedule"`
 }
 
 func (b *BulkApi) CreateBulkSync(ctx context.Context, sync BulkSyncRequest) (*BulkSyncResponse, error) {
@@ -144,4 +143,36 @@ func (b *BulkApi) GetBulkSync(ctx context.Context, id string) (*BulkSyncResponse
 	}
 
 	return &bulk, nil
+}
+
+func (b *BulkApi) GetBulkSyncSchemas(ctx context.Context, id string) ([]BulkSchema, error) {
+	var schemas []BulkSchema
+	result := topLevelResult{Result: &schemas}
+	err := b.client.newRequest(fmt.Sprintf("/api/bulk/syncs/%s/schemas", id)).
+		ToJSON(&result).
+		Fetch(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return schemas, nil
+}
+
+func (b *BulkApi) UpdateBulkSyncSchemas(ctx context.Context, id string, schemas []BulkSchema) error {
+	return b.client.newRequest(fmt.Sprintf("/api/bulk/syncs/%s/schemas", id)).
+		Patch().
+		BodyJSON(&schemas).
+		Fetch(ctx)
+}
+
+func (b *BulkApi) GetBulkSyncSchema(ctx context.Context, id, schemaID string, schema BulkSchema) (*BulkSchema, error) {
+	var result BulkSchema
+	err := b.client.newRequest(fmt.Sprintf("/api/bulk/syncs/%s/schemas/%s", id, schemaID)).
+		ToJSON(&result).
+		Fetch(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &result, nil
 }
